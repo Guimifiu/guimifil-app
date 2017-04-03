b932f3fea0ec4babf383a2da3b291dc5528cdb9e
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2017-04-03 02:01:43 -0300

    Adding menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MenuTabsPage } from '../pages/menu-tabs/menu-tabs';
+import { HomePage } from '../pages/home/home';
+import { GasStationsListPage } from '../pages/gas-stations-list/gas-stations-list';
+import { UserProfilePage } from '../pages/user-profile/user-profile';
 
 
 @Component({
@@ -11,13 +14,34 @@ import { MenuTabsPage } from '../pages/menu-tabs/menu-tabs';
 })
 export class MyApp {
   rootPage:any = MenuTabsPage;
+  @ViewChild(Nav) nav: Nav;
+  
+  pages: Array<{title: string, component: any}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Postos de Gasolina', component: GasStationsListPage },
+      { title: 'Meu Perfil', component: UserProfilePage}
+    ];
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
   }
 }
