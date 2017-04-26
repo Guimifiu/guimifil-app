f392b90718bc48b1e3acf4e22f725860121307a9
Marco Silva <marcowilliamdf@gmail.com>
2017-04-26 15:00:52 -0300

    Fix user provider test
            - Added env files to git for testing ci
            - Fix travis.yml file

---
+++ src/config/environment-development.ts
@@ -0,0 +1,10 @@
+export const ENV = {
+  PRODUCTION        : true,
+  API_URL           : 'http://c66bf96f.ngrok.io/api/v1/',
+  API_TOKEN         : 'f91e20b036fd41ef906a88f843d2f103',
+  FB_APP_ID         : '287910474973285',
+  GOOGLE_APP_WEB_ID : '167308252367-r96b3ql35kgbvao01tj7em1easa49d6i.apps.googleusercontent.com',
+  FIREBASE_SENDER_ID: '',
+  VARIABLES: {
+  }
+};
