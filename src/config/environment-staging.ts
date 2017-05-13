8cd50e0776d7e304ba869e12ec9b2fa338b0fe38
Marco Silva <marcowilliamdf@gmail.com>
2017-05-13 19:33:34 -0300

    Encrypting environment-staging file

--- src/config/environment-staging.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  API_URL           : 'https://staging-guimifiu.herokuapp.com/api/v1/',
-  API_TOKEN         : '1228685128ad43ce9d6e89512f797931',
-  GOOGLE_APP_WEB_ID : '167308252367-r96b3ql35kgbvao01tj7em1easa49d6i.apps.googleusercontent.com',
-};
