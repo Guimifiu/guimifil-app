0598878d056126fa0207ee4ba77a6006803a6971
Marco Silva <marcowilliamdf@gmail.com>
2017-11-15 14:31:06 -0200

    Change fuel supply history page

--- src/config/environment-development.ts
+++ src/config/environment-development.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-  API_URL           : 'http://9f2b19ad.ngrok.io/api/v1/',
+  API_URL           : 'http://295880ff.ngrok.io/api/v1/',
   API_TOKEN         : 'f91e20b036fd41ef906a88f843d2f103',
   GOOGLE_APP_WEB_ID : '1964421970-acssrlebii8i5pio9q4vb5ma4atscbdd.apps.googleusercontent.com',
 };
--- src/models/fuel-supply.ts
+++ src/models/fuel-supply.ts
@@ -8,6 +8,7 @@ export class FuelSupply {
     boycotted: boolean;
     date: string;
     gas_station_name: string;
+    gas_station_icon: string;
     fuel_type: string;
     fuel_type_translated: string;
     rating_stars: number;
